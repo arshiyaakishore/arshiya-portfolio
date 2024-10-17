c3a2a7bcb1c6b3767af6d31bfe60ef23043aaff5
arshiyaakishore <arshiyakishore@gmail.com>
2024-10-17 10:21:36 +0530

    cv

--- app/page.jsx
+++ app/page.jsx
@@ -23,20 +23,20 @@ const Home = () => {
 
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button 
-                variant="outline" 
-                size="lg" 
-                className="uppercase flex items-center gap-2"
-                > 
-                  <a 
+            <a 
                 href="/cv.pdf" 
                 download 
-                className="uppercase flex items-center gap-2 px-4 py-2 border border-gray-300 rounded-lg text-white hover:bg-gray-700 transition-colors"
-              > 
-                <span> Download CV </span>
-                <FiDownload className="text-xl" />
+                className="flex items-center"
+              >
+                <Button 
+                  variant="outline" 
+                  size="lg" 
+                  className="uppercase flex items-center gap-2"
+                > 
+                  <span> Download CV </span>
+                  <FiDownload className="text-xl" />
+                </Button>
               </a>
-              </Button>
               <div className="mb-8 xl:mb-0">
                 <Social 
                   containerStyles="flex gap-6" 
